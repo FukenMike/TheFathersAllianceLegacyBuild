042aaa6b3359086cc47d1af0433f42057b8ac235
FukenMike <mikegriggs929@gmail.com>
2025-05-18 18:00:39 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,11 +1,1 @@
-
-document.addEventListener('DOMContentLoaded', function () {
-  const toggleButton = document.getElementById('menu-toggle');
-  const menuList = document.querySelector('nav.menu-list');
-
-  if (toggleButton && menuList) {
-    toggleButton.addEventListener('click', () => {
-      menuList.classList.toggle('visible');
-    });
-  }
-});
+// Future interactivity here
